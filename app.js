ff18ca556d08671e93fc40345b3fde49c6f20c72
Jaison Dev <jaison.alievi@hotmail.com>
2020-08-27 14:21:20 -0300

    aula 296 devolvendo json como resposta

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const app = express()
 app.get('/', (req, res)=>{
   res.send('<h1>Minha lista de tarefas!</h1>')
 })
+//devolvendo json no get
+//geralmente devolve um objeto ou array
+.get('/json', (req, res) => {
+  res.json({
+    name: "jaison",
+    idade: 26,
+    profissão: 'programmer'
+})
+})
 .listen(3000, ()=>{
   console.log('server initialized')
 })
