4e1525a0e6f5110164e52cc63b34d82a2ff8b2e5
Jaison Dev <jaison.alievi@hotmail.com>
2020-08-31 13:03:39 -0300

    usando rotas!

--- app.js
+++ app.js
@@ -1,31 +1,12 @@
 const express = require('express')
+const checklistRouter = require('./src/routes/checklist')
 
 const app = express()
-//usando middlewares, por padrão o express ja tras um em json
+
 app.use(express.json())
-//uma função q retorna a resposta deste middleware
-function log(req, res, next) {
-  console.log(req.body)
-  console.log(`Data: ${Date.now()}`)
-  next()
-}
 
-app.use(log) //chamando o log
+app.use(checklistRouter)
 
-//ae vamos no postman e usamos o json e o send
-
-app.get('/', (req, res)=>{
-  res.send('<h1>Minha lista de tarefas!</h1>')
-})
-//devolvendo json no get
-//geralmente devolve um objeto ou array
-.get('/json', (req, res) => {
-  res.json({
-    name: "jaison",
-    idade: 26,
-    profissão: 'programmer'
-})
-})
 .listen(3000, ()=>{
   console.log('server initialized')
 })
---
+++ src/routes/checklist.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/checklist',(req, res) =>{
+  console.log("ola")
+  res.send()
+})
+//continuarei daqui
+module.exports = router
