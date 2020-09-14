41632db988674e22785799c67533a1cb418ab43b
Jaison Dev <jaison.alievi@hotmail.com>
2020-09-14 16:25:45 -0300

    adicionando subtarefas em tarefas

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
-const express = require('express')
-const path = require('path')
-const checklistRouter = require('./src/routes/checklist')
-//pagina teste
-const rootRouter = require('./src/routes/index')
-//method override...o thml 5 n da suporte para methodos PUT/DELETE então é ecessário a instalação deste method
-const methodOverride = require('method-override')
-//aqui imortamos as configs do mongoose 
-require('./config/database')
+const express = require('express');
+const path = require('path');
 
-const app = express()
-app.use(express.json())
+const checkListRouter = require('./src/routes/checklist');
+const taskRouter = require('./src/routes/task');
+
+//pagina teste
+const rootRouter = require('./src/routes/index');
+
+//method override...o thml 5 n da suporte para methodos PUT/DELETE então é ecessário a instalação deste method
+const methodOverride = require('method-override');
+
+//aqui imortamos as configs do mongoose 
+require('./config/database');
+
+
+const app = express();
+app.use(express.json());
 
 //habilitando uso de url de forms
-app.use(express.urlencoded({extended:true}))
-
-//habilitando uso de arquivos estaticos
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }));
 
 //method override
 //aqui eu to falando para o metodo override sobrescrever os padrões post e get
-app.use(methodOverride('_method', {methods:['POST', 'GET']}))//atributo para selecionar o tipo de metodo
+app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
+
+//habilitando uso de arquivos estaticos
+app.use(express.static(path.join(__dirname, 'public')));
 
 //adicionamos o path para falar onde estão nossas views para uso do ejs!
-app.set('views', path.join(__dirname, 'src/views'))
+app.set('views', path.join(__dirname, 'src/views'));
 
 //agora irei instalar a minha view engine
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 //added rota para teste com o ejs
-app.use('/', rootRouter)
-app.use('/checklists',checklistRouter)
+app.use('/', rootRouter);
+app.use('/checklists', checkListRouter);
+app.use('/checklists', taskRouter.checklistDepedent);
 
-app.listen(3000, ()=>{
-  console.log('server initialized')
+app.listen(3000, () => {
+  console.log('Server initialized');
 })
+
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,4 +1,4 @@
-const express = require('express')
+/*const express = require('express')
 
 const router = express.Router()
 
@@ -29,7 +29,7 @@ router.get('/new', async(req, res)=>{
 //rota de edição
 router.get('/:id/edit', async(req, res)=>{
   try {
-    let checklist = await Checklist.findById(req.params.id)
+    let checklist = await (await Checklist.findById(req.params.id)).populate('tasks')//igual ao agregate, é uma forma de relacionar
     res.status(200).render('checklists/edit', {checklist:checklist})
   } catch (error) {
     res.status(500).render('pages/error', {error:'erro ao exibir a lista de tarefas'})
@@ -95,4 +95,83 @@ router.delete('/:id', async (req, res) => {
 })
 
 //basta
-module.exports = router
+module.exports = router /*/
+
+const express = require('express');
+
+const router = express.Router();
+
+const Checklist = require('../models/checklist');
+
+router.get('/', async (req, res) => {
+  try {
+    let checklists = await Checklist.find({});
+    res.status(200).render('checklists/index', { checklists: checklists})
+  } catch (error) {
+    res.status(200).render('pages/error', {error: 'Erro ao exibir as Listas'});
+  }
+})
+
+router.get('/new', async(req, res) => {
+  try {
+    let checklist = new Checklist();
+    res.status(200).render('checklists/new', { checklist: checklist });
+  } catch (error) {
+    res.status(500).render('pages/error', { errors: 'Erro ao carregar o formulário' })
+  }
+})
+
+router.get('/:id/edit', async(req, res) =>{
+  try {
+    let checklist = await Checklist.findById(req.params.id);
+    res.status(200).render('checklists/edit', { checklist: checklist })
+  } catch (error) {
+    res.status(500).render('pages/error', {error: 'Erro ao exibir a edição Listas de tarefas'});
+  }
+})
+
+router.post('/', async (req, res) => {
+  let { name } = req.body.checklist;
+  let checklist = new Checklist({name})
+
+  try {
+    await checklist.save();
+    res.redirect('/checklists');
+  } catch (error) {
+    res.status(422).render('checklists/new', { checklist: { ...checklist, error}})
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id).populate('tasks');
+    res.status(200).render('checklists/show', { checklist: checklist})
+  } catch (error) {
+    res.status(500).render('pages/error', {error: 'Erro ao exibir as Listas de tarefas'});
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  let { name } = req.body.checklist;
+  let checklist = await Checklist.findById(req.params.id);
+
+  try {
+    await checklist.update({name});
+    res.redirect('/checklists');
+  } catch (error) {
+    let errors = error.errors;
+    res.status(422).render('checklists/edit', {checklist: {...checklist, errors}});
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    let checklist = await Checklist.findByIdAndRemove(req.params.id);
+    res.redirect('/checklists');
+  } catch (error) {
+    res.status(500).render('pages/error', {error: 'Erro ao delete a Lista de tarefas'});
+  }
+})
+
+  
+module.exports = router;
